552c6ecbeb9b31ce82c8db70f9f04bd937aa3772
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-08 19:57:58 -0400

    added fix modules

---
+++ models/Thought.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      // Mongoose's ObjectId data type
+      type: Schema.Types.ObjectId,
+      // Default value is set to a new ObjectId
+      default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      // Set default value to the current timestamp
+      default: Date.now,
+      // Use a getter method to format the timestamp on query
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema], // Array of nested documents created with reactionSchema
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true,
+  },
+  id: false,
+});
+
+// Define the virtual field "reactionCount"
+thoughtSchema.virtual('reactionCount').get(function () {
+  // 'this' refers to the current document
+  return this.reactions.length;
+});
+
+// Create the Thought model
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
