b5dda61ee08f4c4ffae9a81cf08d5e064d268407
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-17 16:11:30 -0400

    created getUserByID

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,14 @@ const userController =  {
     .catch((err) => res.status(500).json(err));
   },
   async getUserById(req, res) {
-
+    User.findOne({ _id: req.params.userId })
+    .select('-__v')
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: 'No user with that ID' })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
   },
   async createUser(req, res) {
 
