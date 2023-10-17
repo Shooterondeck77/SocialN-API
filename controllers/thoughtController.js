0e0aa1fbab5684a6b67b21754c400dd4e3a60dc2
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-17 16:15:14 -0400

    setup getThoughtById

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,7 +7,13 @@ const thoughtController =  {
     .catch((err) => res.status(500).json(err));
   },
   async getThoughtById(req, res) {
-
+    Thought.findOne({ _id: req.params.thoughtId })
+    .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: 'No Thought with that ID' })
+        : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
   },
   async createThought(req, res) {
 
