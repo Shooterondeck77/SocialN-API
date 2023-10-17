78df44889cc448b791a40e654df85707a8277321
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-17 16:13:00 -0400

    set up addFriend

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,7 +58,17 @@ const userController =  {
     .catch((err) => res.status(500).json(err));
   },
   async addFriend(req, res) {
-
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No User with this id!' })
+          : res.json(User)
+      )
+      .catch((err) => res.status(500).json(err));
   },
   async removeFriend(req, res) {
 
