a32f3c35fdb54cc813faa871584c4630187e00b8
Shooterondeck77 <timothytaffur@gmail.com>
2023-10-17 16:13:36 -0400

    set up remove friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,25 @@
-const {Thought, User} = require("../models")
+const { Thought, User } = require("../models")
 
-const userController =  {
+const userController = {
   async getAllUser(req, res) {
     User.find()
-    .then((users) => res.json(users))
-    .catch((err) => res.status(500).json(err));
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
   },
   async getUserById(req, res) {
     User.findOne({ _id: req.params.userId })
-    .select('-__v')
-    .then((user) =>
-      !user
-        ? res.status(404).json({ message: 'No user with that ID' })
-        : res.json(user)
-    )
-    .catch((err) => res.status(500).json(err));
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
   },
   async createUser(req, res) {
     User.create(req.body)
-    .then((dbUserData) => res.json(dbUserData))
-    .catch((err) => res.status(500).json(err));
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
   },
   async updateUser(req, res) {
     User.findOneAndUpdate(
@@ -39,23 +39,23 @@ const userController =  {
   },
   async deleteUser(req, res) {
     User.findOneAndRemove({ _id: req.params.userId })
-    .then((user) =>
-      !user
-        ? res.status(404).json({ message: 'No User with this id!' })
-        : User.findOneAndUpdate(
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No User with this id!' })
+          : User.findOneAndUpdate(
             { users: req.params.userId },
             { $pull: { Users: req.params.userId } },
             { new: true }
           )
-    )
-    .then((user) =>
-      !user
-        ? res
+      )
+      .then((user) =>
+        !user
+          ? res
             .status(404)
             .json({ message: 'User created but no user with this id!' })
-        : res.json({ message: 'User successfully deleted!' })
-    )
-    .catch((err) => res.status(500).json(err));
+          : res.json({ message: 'User successfully deleted!' })
+      )
+      .catch((err) => res.status(500).json(err));
   },
   async addFriend(req, res) {
     User.findOneAndUpdate(
@@ -71,7 +71,17 @@ const userController =  {
       .catch((err) => res.status(500).json(err));
   },
   async removeFriend(req, res) {
-
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No User with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
   },
 }
 
